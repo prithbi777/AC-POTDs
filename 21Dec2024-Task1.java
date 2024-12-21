559ab6ce4c06c67bc422b1de4b93abb61eada135
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2024-12-21 14:24:35 +0530

    21Dec2024-Task1.java

---
+++ 21Dec2024-Task1.java
@@ -0,0 +1,17 @@
+/*
+Author: Prithbiraj Mahanta
+Date: 21-12-2024
+Platform: Leetcode
+Problem: 3110
+
+*/
+
+class Solution {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for(int i=0;i<s.length()-1;i++){
+            sum += Math.abs(s.charAt(i)-s.charAt(i+1));
+        }
+        return sum;
+    }
+}
