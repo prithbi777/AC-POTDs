62328f0a10cc18c0e24d9421c2511bbea9a58517
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2024-12-24 01:47:25 +0530

    Update 24Dec2024-Task1.java

--- 24Dec2024-Task1.java
+++ 24Dec2024-Task1.java
@@ -7,7 +7,7 @@ Problem: 75
 
 */
 
-
+//THIS ALGORITHM IS CALLED DUTCH NATIONAL FLAG ALGORITHM
 class Solution {
     public void sortColors(int[] nums) {
         int start = 0, mid = 0, end = nums.length-1;
