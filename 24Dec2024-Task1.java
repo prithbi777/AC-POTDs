9744b791eb2233d8c196316527780df773ade8b2
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2024-12-24 01:46:44 +0530

    Add files via upload

---
+++ 24Dec2024-Task1.java
@@ -0,0 +1,33 @@
+/*
+Author: Prithbiraj Mahanta
+Date: 24-12-2024
+TaskNo: 1
+Platform: Leetcode
+Problem: 75
+
+*/
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int start = 0, mid = 0, end = nums.length-1;
+        while(mid<=end){
+            if(nums[mid]==0){
+                int temp = nums[mid];
+                nums[mid] = nums[start];
+                nums[start] = temp;
+                mid++;
+                start++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid] = nums[end];
+                nums[end] = temp;
+                end--;
+            }
+        }
+        
+       
+    }
+}
