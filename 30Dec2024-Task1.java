67f5463a1fc087e29f5f698853532ca39710aa76
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2024-12-31 20:22:01 +0530

    Update 30Dec2024-Task1.java

--- 30Dec2024-Task1.java
+++ 30Dec2024-Task1.java
@@ -8,31 +8,55 @@ Problem: 238
 */
 
 
-
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] answer = new int[n];
+        ////mine
+    //     int[] prefixmul = new int[nums.length];
+    //   int[] suffixmul = new int[nums.length];
+    //   prefixmul[0] = 1;
+    //   suffixmul[nums.length-1] = 1;
+    //   for(int i=1;i<nums.length;i++){
+    //     prefixmul[i] = prefixmul[i-1]*nums[i-1];
+    //   }
+    //   for(int i=nums.length-2;i>=0;i--){
+    //       suffixmul[i] = suffixmul[i+1]*nums[i+1];
+    //   }
+    //   int[] ans = new int[nums.length];
+    //   for(int i=0;i<nums.length;i++){
+    //     ans[i] = suffixmul[i]*prefixmul[i];
+    //   }
+    // return ans;
 
-        // Step 1: Calculate left products
-        answer[0] = 1; // No elements to the left of the first element
-        for (int i = 1; i < n; i++) {
-            answer[i] = answer[i - 1] * nums[i - 1];
+
+
+
+
+
+
+    // OPTIMIZED: space O(1)
+   
+        int[] ans = new int[nums.length];
+
+        // Step 1: Initialize the answer array with prefix products
+        ans[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
 
-        // Step 2: Calculate right products and multiply with left products
-        int right = 1; // No elements to the right of the last element
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= right; // Multiply with the right product
-            right *= nums[i];  // Update the right product
+        // Step 2: Calculate suffix products and update the answer array
+        int suffix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= suffix;
+            suffix *= nums[i];
         }
 
-        return answer;
+        // Print the result
+        return ans;
+
+
+
+
     }
 }
-
-
-
-
 
 
