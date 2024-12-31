469366396a6d334dd4ea67590e43e8f9dca7627b
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2024-12-31 21:21:36 +0530

    Update N30Dec2024-Task2.java

--- N30Dec2024-Task2.java
+++ N30Dec2024-Task2.java
@@ -6,3 +6,16 @@ Platform: Leetcode
 Problem: 1689
 
 */
+
+
+
+class Solution {
+    public int minPartitions(String n) {
+       int ans = 0;
+       for(int i=0;i<n.length();i++){
+          ans = Math.max(ans, n.charAt(i)-'0');
+          if (ans == 9) break;
+       }
+       return ans;
+    }
+}
