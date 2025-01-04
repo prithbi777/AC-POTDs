92e4898042a84f4cdd87babfcaf63f6cf7c1a6c1
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-04 23:51:56 +0530

    Create N04Jan2025-Task2.java

---
+++ N04Jan2025-Task2.java
@@ -0,0 +1,8 @@
+/*
+Author: Prithbiraj Mahanta
+Date: 04-01-2025
+TaskNo: 2
+Platform: Leetcode
+Problem: https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/description/
+
+*/
