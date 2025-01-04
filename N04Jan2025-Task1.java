07e8eb59e1417e47a40a00af3de5da735f884e57
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-04 23:51:02 +0530

    Create N04Jan2025-Task1.java

---
+++ N04Jan2025-Task1.java
@@ -0,0 +1,8 @@
+/*
+Author: Prithbiraj Mahanta
+Date: 04-01-2025
+TaskNo: 1
+Platform: Leetcode
+Problem: https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
+
+*/
