f5bde5a6bf10acb74b93d70da27cf1529e0c928c
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-07 22:26:30 +0530

    Update N07Jan2025-Task1.java

--- N07Jan2025-Task1.java
+++ N07Jan2025-Task1.java
@@ -10,3 +10,35 @@ Problem: https://leetcode.com/problems/arranging-coins/description/
 
 
 
+
+class Solution {
+    public int arrangeCoins(int n) {
+
+        // //brute force 
+        // int completeRows = 0,i = 1;
+        // while(i<=n){
+        //     completeRows++;
+        //     n -= i;
+        //     i++;
+        // }
+        // return completeRows;
+
+
+
+        //optimized(using bs)
+        long left = 1, right = n;
+        while (left<=right) {
+            long mid = left + (right-left)/2;
+            long sum = (mid*(mid+1))/2;
+            if(sum>n){
+                right = mid -1; 
+            }else{
+                left = mid +1;
+            }
+        }
+        return (int)right;
+    }
+}
+
+
+
