a94e689ef14e2de0cbafaeecbf4a027a82db1f66
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-09 03:50:24 +0530

    Create 09Jan2025-Task1.java

---
+++ 09Jan2025-Task1.java
@@ -0,0 +1,40 @@
+/*
+Author: Prithbiraj Mahanta
+Date: 09-01-2025
+TaskNo: 1
+Platform: LT
+Problem: https://leetcode.com/problems/find-peak-element/
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        int left = 1, right = n-2;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+               return mid;
+            }else if(nums[mid]>nums[mid-1]){
+                left = mid+1;
+            }else{
+                right = mid -1;
+            }
+        }
+        return 0;
+    }
+}
